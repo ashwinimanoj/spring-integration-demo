f299a406a3d6f8436e06e3968243676630f043e1
Ashwini Manoj <ashwinimanoj@gmail.com>
2020-02-01 19:43:10 +0530

    Add Book class

---
+++ src/main/java/com/example/demo/Book.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+public class Book {
+
+    private long bookId;
+    private String title;
+    private Genre genre;
+
+    public enum Genre {
+        fantasy, horror, romance, thriller
+
+    }
+
+    public Book(long bookId, String title, Genre genre) {
+        this.bookId = bookId;
+        this.title = title;
+        this.genre = genre;
+    }
+
+}
